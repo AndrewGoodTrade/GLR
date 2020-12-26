7cef07a4d94775404a716ec74f1d327bf0139804
JamesAlexander-rockwall <james.alexander.rockwall@gmail.com>
2020-12-26 18:35:28 +0200

    feat - import firebase module and implement login page

--- readMe(Andrew).txt
+++
@@ -1,7 +0,0 @@
-Hi, Andrew.
-Could we chat via skype?
-I think skype is better than freelancer to communicate easily.
-Please don't mention about the skype in the freelancer because outside communication is not
-allowed.
-My skype id is live:mungun105
-Best regards.
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+
+import { environment } from '../environments/environment';
+import { AuthService } from './provider/auth.service';
+import { LoginPageComponent } from './login-page/login-page.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
-  providers: [],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
